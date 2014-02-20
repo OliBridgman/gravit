840a55a45fc4611aa026eef52549de5721de07f7
Anna Ignatenkova <ann.ign@mail.ru>
2014-02-20 17:12:17 +0300

    Quick fix for Path #1 test inside.

--- src/development/test/path/create_path_1.js
+++ src/development/test/path/create_path_1.js
@@ -2,7 +2,8 @@
     function test(scene, page, layer, view) {
         var path = new GXPath();
         path.setProperty('closed', true);
-
+        /*
+        // path orientation 1
         var ap = new GXPathBase.AnchorPoint();
         ap.setProperties(['x', 'y', 'hrx', 'hry'], [71.2795452,261.141279, 251.140545,393.280279]);
         path.getAnchorPoints().appendChild(ap);
@@ -27,6 +28,33 @@
 
         ap = path.getAnchorPoints().getFirstChild();
         ap.setProperties(['hlx', 'hly'], [-108.581455,129.002279]);
+        */
+
+        // path orientation 2
+        var ap = new GXPathBase.AnchorPoint();
+        ap.setProperties(['x', 'y', 'hrx', 'hry'], [71.2795452,261.141279, -108.581455,129.002279]);
+        path.getAnchorPoints().appendChild(ap);
+
+        var cProps = ['hlx', 'hly', 'x', 'y', 'hrx', 'hry'];
+
+        ap = new GXPathBase.AnchorPoint();
+        ap.setProperties(cProps, [114.280545,189.698279, 108.175545,132.106279, 97.0715452,27.3492795]);
+        path.getAnchorPoints().appendChild(ap);
+
+        ap = new GXPathBase.AnchorPoint();
+        ap.setProperties(cProps, [259.140545,204.106279, 213.210545,51.0722795, 175.470545,-74.6727205]);
+        path.getAnchorPoints().appendChild(ap);
+
+        ap = new GXPathBase.AnchorPoint();
+        ap.setProperties(cProps, [438.309545,58.4932795, 480.314545,207.210279, 516.175545,334.176279]);
+        path.getAnchorPoints().appendChild(ap);
+
+        ap = new GXPathBase.AnchorPoint();
+        ap.setProperties(cProps, [335.748545,435.129279, 290.383545,308.106279, 250.383545,196.106279]);
+        path.getAnchorPoints().appendChild(ap);
+
+        ap = path.getAnchorPoints().getFirstChild();
+        ap.setProperties(['hlx', 'hly'], [251.140545,393.280279]);
 
         //var sGroup = new GXVectorTransformStyle();
         //sGroup.appendChild(new GXPaintContourStyle());
