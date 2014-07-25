b4f2f089d648f5c35d2a707ebe3f038e95192d1e
Alexander Adam <aadam@quasado.com>
2014-07-25 12:07:53 +0200

    fix formatNumber

--- src/infinity/core/util.js
+++ src/infinity/core/util.js
@@ -232,84 +232,11 @@
      * @param {Number} number the number to format
      * @param {Number} [decimalPlaces] the number of decimal places,
      * defaults to 3
-     * @param {String} [decimalSeparator] the decimal separator to be used,
-     * defaults to ','
-     * @param {String} [thousandSeparator] the thousands separator, defaults
-     * to empty (= no separator)
-     * @param {Boolean} [includeEndingZeros] if true, ending zeros until the given
-     * number of decimal places is reached will be added, otherwise any ending
-     * decimal places with zero will be ignored. Defaults to false.
      * @returns {string}
      */
-    IFUtil.prototype.formatNumber = function (number, decimalPlaces, decimalSeparator, thousandSeparator, includeEndingZeros) {
-        // zero is a special case
-        if (number === 0) {
-            return '0';
-        }
-
-        decimalPlaces = decimalPlaces || 3;
-        decimalSeparator = decimalSeparator || ',';
-        thousandSeparator = thousandSeparator || '';
-
-        var sinal = 1;
-        if (number < 0) {
-            number = -number;
-            sinal = -1;
-        }
-
-        var resposta = "";
-        var part = "";
-        if (number != Math.floor(number)) {
-            part = Math.round((number - Math.floor(number)) * Math.pow(10, decimalPlaces)).toString();
-
-            while (part.length < decimalPlaces) {
-                part = '0' + part;
-            }
-
-            if (!includeEndingZeros && part.length) {
-                // remove ending zero if any
-                var tmpPart = '';
-                for (var i = part.length - 1; i >= 0; --i) {
-                    var char = part.charAt(i);
-                    if (char === '0' && !tmpPart.length) {
-                        continue;
-                    }
-                    tmpPart = char + tmpPart;
-                }
-                part = tmpPart;
-            }
-
-            if (decimalPlaces > 0 && part.length) {
-                resposta = decimalSeparator + part;
-                number = Math.floor(number);
-            } else {
-                number = Math.round(number);
-            }
-        }
-
-        // take care - zero is a special case
-        if (number === 0) {
-            resposta = '0' + resposta;
-        } else
-            while (number > 0) {
-                part = (number - Math.floor(number / 1000) * 1000).toString();
-                number = Math.floor(number / 1000);
-                if (number > 0) {
-                    while (part.length < 3) {
-                        part = '0' + part;
-                    }
-                }
-                resposta = part + resposta;
-                if (number > 0) {
-                    resposta = thousandSeparator + resposta;
-                }
-            }
-
-        if (sinal < 0) {
-            resposta = '-' + resposta;
-        }
-
-        return resposta;
+    IFUtil.prototype.formatNumber = function (number, decimalPlaces) {
+        decimalPlaces = typeof decimalPlaces === 'number' ? decimalPlaces : 3;
+        return parseFloat(number.toFixed(decimalPlaces)).toString();
     };
 
     /**
