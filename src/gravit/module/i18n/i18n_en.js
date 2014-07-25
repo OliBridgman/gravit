d5fa046e4b77d523df84a173f26f371aa918861c
Alexander Adam <aadam@quasado.com>
2014-07-25 15:39:53 +0200

    Refactoring to split gravit workspace and gravit core module code

---
+++ src/gravit/module/i18n/i18n_en.js
@@ -0,0 +1,58 @@
+// Action
+ifLocale.setValues(GAddLayerAction, IFLocale.Language.English, ["title"], ["Add Layer"]);
+ifLocale.setValues(GAddPageAction, IFLocale.Language.English, ["title"], ["Add Page"]);
+ifLocale.setValues(GAlignAction, IFLocale.Language.English, ["title.align-left", "title.align-center", "title.align-right", "title.align-top", "title.align-middle", "title.align-bottom", "title.distribute-horizontal", "title.distribute-vertical"], ["Align Left", "Align Center", "Align Right", "Align Top", "Align Middle", "Align Bottom", "Distribute Horizontally", "Distribute Vertically"]);
+ifLocale.setValues(GCloneAction, IFLocale.Language.English, ["title"], ["Clone"]);
+ifLocale.setValues(GCloseAction, IFLocale.Language.English, ["title"], ["Close"]);
+ifLocale.setValues(GCloseAllAction, IFLocale.Language.English, ["title"], ["Close All"]);
+ifLocale.setValues(GCopyAction, IFLocale.Language.English, ["title"], ["Copy"]);
+ifLocale.setValues(GCopyAttributesAction, IFLocale.Language.English, ["title"], ["Copy Attributes"]);
+ifLocale.setValues(GCutAction, IFLocale.Language.English, ["title"], ["Cut"]);
+ifLocale.setValues(GDeleteAction, IFLocale.Language.English, ["title"], ["Delete"]);
+ifLocale.setValues(GDeleteLayerAction, IFLocale.Language.English, ["title"], ["Delete Layer"]);
+ifLocale.setValues(GDeletePageAction, IFLocale.Language.English, ["title"], ["Delete Page"]);
+ifLocale.setValues(GDuplicateAction, IFLocale.Language.English, ["title"], ["Duplicate"]);
+ifLocale.setValues(GExportAction, IFLocale.Language.English, ["title"], ["%name%..."]);
+ifLocale.setValues(GFitAllAction, IFLocale.Language.English, ["title"], ["Fit All"]);
+ifLocale.setValues(GFitCurrentLayerAction, IFLocale.Language.English, ["title"], ["Fit Layer"]);
+ifLocale.setValues(GFitCurrentPageAction, IFLocale.Language.English, ["title"], ["Fit Page"]);
+ifLocale.setValues(GFitSelectionAction, IFLocale.Language.English, ["title"], ["Fit Selection"]);
+ifLocale.setValues(GGroupAction, IFLocale.Language.English, ["title"], ["Group Selection"]);
+ifLocale.setValues(GImportAction, IFLocale.Language.English, ["title"], ["%name%..."]);
+ifLocale.setValues(GInvertSelectionAction, IFLocale.Language.English, ["title"], ["Invert Selection"]);
+ifLocale.setValues(GLayerTypeAction, IFLocale.Language.English, ["title"], ["%name% Layer"]);
+ifLocale.setValues(GNewAction, IFLocale.Language.English, ["title"], ["New..."]);
+ifLocale.setValues(GNewWindowAction, IFLocale.Language.English, ["title"], ["New Window"]);
+ifLocale.setValues(GOpenAction, IFLocale.Language.English, ["title", "title-default"], ["From %name%...", "Open..."]);
+ifLocale.setValues(GOrderAction, IFLocale.Language.English, ["title.send-front", "title.bring-forward", "title.send-backward", "title.send-back"], ["Send To Front", "Bring Forward", "Send Backward", "Send To Back"]);
+ifLocale.setValues(GOriginalViewAction, IFLocale.Language.English, ["title"], ["Original-View"]);
+ifLocale.setValues(GPaintModeAction, IFLocale.Language.English, ["title"], ["%name% View"]);
+ifLocale.setValues(GPasteAction, IFLocale.Language.English, ["title"], ["Paste"]);
+ifLocale.setValues(GPasteInsideAction, IFLocale.Language.English, ["title"], ["Paste Inside Selection"]);
+ifLocale.setValues(GPasteAttributesAction, IFLocale.Language.English, ["title"], ["Paste Attributes"]);
+ifLocale.setValues(GPixelPreviewAction, IFLocale.Language.English, ["title"], ["Pixel Preview"]);
+ifLocale.setValues(GRedoAction, IFLocale.Language.English, ["title"], ["Redo"]);
+ifLocale.setValues(GSaveAction, IFLocale.Language.English, ["title"], ["Save"]);
+ifLocale.setValues(GSaveAllAction, IFLocale.Language.English, ["title"], ["Save All"]);
+ifLocale.setValues(GSaveAsAction, IFLocale.Language.English, ["title", "title-default"], ["To %name%...", "Save As..."]);
+ifLocale.setValues(GShowRulersAction, IFLocale.Language.English, ["title"], ["Rulers"]);
+ifLocale.setValues(GSelectAllAction, IFLocale.Language.English, ["title"], ["Select All"]);
+ifLocale.setValues(GShowAllPagesAction, IFLocale.Language.English, ["title"], ["All Pages"]);
+ifLocale.setValues(GSnapUnitAction, IFLocale.Language.English, ["title.full", "title.half"], ["Snap to full units", "Snap to half units"]);
+ifLocale.setValues(GUndoAction, IFLocale.Language.English, ["title"], ["Undo"]);
+ifLocale.setValues(GUngroupAction, IFLocale.Language.English, ["title"], ["Ungroup Selection"]);
+ifLocale.setValues(GZoomInAction, IFLocale.Language.English, ["title"], ["Zoom in"]);
+ifLocale.setValues(GZoomOutAction, IFLocale.Language.English, ["title"], ["Zoom out"]);
+
+// Color
+ifLocale.setValues(GAnalogousMatcher, IFLocale.Language.English, ["title"], ["Analogous"]);
+ifLocale.setValues(GComplementaryMatcher, IFLocale.Language.English, ["title"], ["Complementary"]);
+
+// Palette
+ifLocale.setValues(GPropertiesPalette, IFLocale.Language.English, ["title"], ["Properties"]);
+ifLocale.setValues(GStylePalette, IFLocale.Language.English, ["title"], ["Style"]);
+ifLocale.setValues(GTransformPalette, IFLocale.Language.English, ["title"], ["Transform"]);
+
+// Sidebar
+ifLocale.setValues(GStylesSwatchesSidebar, IFLocale.Language.English, ["title"], ["Styles & Swatches"]);
+ifLocale.setValues(GPagesLayersSidebar, IFLocale.Language.English, ["title"], ["Pages & Layers"]);
