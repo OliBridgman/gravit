9bddd69cc31a6d4093c62d4189e0be9355ccd31c
Alexander Adam <aadam@quasado.com>
2014-10-11 11:39:23 +0200

    Add styling to ShapeSet

--- src/infinity/scene/shape/shapeset.js
+++ src/infinity/scene/shape/shapeset.js
@@ -5,16 +5,25 @@
      * @extends IFItem
      * @mixes IFNode.Container
      * @mixes IFElement.Transform
+     * @mixes IFElement.Stylable
      * @constructor
      */
     function IFShapeSet() {
         IFItem.call(this);
     }
-    IFNode.inheritAndMix('shapeSet', IFShapeSet, IFItem, [IFNode.Container, IFElement.Transform]);
+    IFNode.inheritAndMix('shapeSet', IFShapeSet, IFItem, [IFNode.Container, IFElement.Transform, IFElement.Stylable]);
 
     /** @override */
     IFShapeSet.prototype.validateInsertion = function (parent, reference) {
         return parent instanceof IFLayer || parent instanceof IFShapeSet;
+    };
+
+    /** @override */
+    IFShapeSet.prototype._paintStyleLayer = function (context, layer) {
+        if (layer === IFStylable.Layer.Background) {
+            // Paint children content
+            this._paintChildren(context);
+        }
     };
 
     /** @override */
