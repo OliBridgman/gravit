bcaf208225cdc30de99f9ee8fa447e6987eeebfb
Anna Ignatenkova <ann.ign@mail.ru>
2014-10-26 03:36:28 +0300

    Temporal fix for the issue with svg import of gravit toolbar icon

--- src/development/test/place_svg.js
+++ src/development/test/place_svg.js
@@ -8,9 +8,24 @@
                     if (value) {
                         var doc = gApp.getActiveDocument();
                         var page = doc.getScene().getActivePage();
+                        var editor = doc.getEditor();
                         GIO.read('test.svg', value, function (node) {
                             if (node) {
-                                doc.getScene().getActiveLayer().appendChild(node);
+                                if (node instanceof GElement) {
+                                    editor.insertElements([node], true, true);
+                                    // TODO: find the reason of the issue with bbox when group from only compound path is inserted
+                                    // and then remove the below temporal fix for the issue
+                                    if (node instanceof GGroup) {
+                                        var groupChildren = node.getChildren();
+                                        for (var j = 0; j < groupChildren.length; ++j) {
+                                            var child = groupChildren[j];
+                                            child.setFlag(GNode.Flag.Selected);
+                                            child.removeFlag(GNode.Flag.Selected);
+                                        }
+                                    }
+                                } else {
+                                    doc.getScene().getActiveLayer().appendChild(node);
+                                }
                             }
                         }, {
                             baseWidth: page.getProperty('w'),
