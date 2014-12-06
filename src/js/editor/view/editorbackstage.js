be3b1864f41c0d4daa9e1eff99fc4775299e007e
Alexander Adam <aadam@quasado.com>
2014-12-06 14:22:14 +0100

    Proper page decoration painting

--- src/js/editor/view/editorbackstage.js
+++ src/js/editor/view/editorbackstage.js
@@ -15,8 +15,6 @@
 
     GObject.inherit(GEditorBackStage, GStage);
 
-    GEditorBackStage.MARGIN_OUTLINE = new GRGBColor([255, 0, 255]);
-
     /** @override */
     GEditorBackStage.prototype.release = function () {
         this._view.getScene().removeEventListener(GScene.InvalidationRequestEvent, this._sceneInvalidationRequest, this);
@@ -24,17 +22,6 @@
 
     /** @override */
     GEditorBackStage.prototype.paint = function (context) {
-        // View painting
-
-        //
-        // Scene painting
-        //
-
-        // Transform dirty areas into scene
-        if (context.dirtyMatcher) {
-            context.dirtyMatcher.transform(this._view.getViewTransform());
-        }
-
         var scene = this._view.getScene();
 
         if (scene instanceof GPage) {
@@ -52,8 +39,6 @@
     GEditorBackStage.prototype._sceneInvalidationRequest = function (event) {
         var area = event.area;
         if (area) {
-            // Ensure to map the scene area into view coordinates, first
-            // TODO : How to handle view margins!?
             area = this._view.getWorldTransform().mapRect(area);
         }
         this.invalidate(area);
@@ -61,8 +46,6 @@
 
     /**
      * @param context
-     * @param transform
-     * @param page
      * @private
      */
     GEditorBackStage.prototype._renderPageDecoration = function (context) {
@@ -70,11 +53,8 @@
         var transform = this._view.getWorldTransform();
         // Get page rectangle and transform it into world space
         var pageRect = new GRect(page.getProperty('x'), page.getProperty('y'), page.getProperty('w'), page.getProperty('h'));
-        var marginRect = pageRect.expanded(-page.getProperty('ml'), -page.getProperty('mt'), -page.getProperty('mr'), -page.getProperty('mb'));
         var transformedPageRect = transform.mapRect(pageRect).toAlignedRect();
-        var transformedMarginRect = transform.mapRect(marginRect).toAlignedRect();
         var x = transformedPageRect.getX(), y = transformedPageRect.getY(), w = transformedPageRect.getWidth(), h = transformedPageRect.getHeight();
-        var mx = transformedMarginRect.getX(), my = transformedMarginRect.getY(), mw = transformedMarginRect.getWidth(), mh = transformedMarginRect.getHeight();
 
         // Paint page color or chessboard if transparent
         if (!PAGE_CHESSBOARD_FILL) {
@@ -85,11 +65,6 @@
         context.canvas.setTransform(new GTransform(1, 0, 0, 1, x, y));
         context.canvas.fillRect(0, 0, w, h, chessFill);
         context.canvas.resetTransform();
-
-        // Paint margin rect
-        if (!GRect.equals(pageRect, marginRect)) {
-            context.canvas.strokeRect(mx + 0.5, my + 0.5, mw, mh, 1, GEditorBackStage.MARGIN_OUTLINE);
-        }
     };
 
     /** @override */
--- src/js/editor/view/editorfrontstage.js
+++ src/js/editor/view/editorfrontstage.js
@@ -8,14 +8,18 @@
      */
     function GEditorFrontStage(view) {
         GStage.call(this, view);
+        view.getScene().addEventListener(GScene.InvalidationRequestEvent, this._sceneInvalidationRequest, this);
         view.getScene().addEventListener(GNode.AfterPropertiesChangeEvent, this._sceneAfterPropertiesChanged, this);
         view.addEventListener(GMouseEvent.Release, this._cleanGuides, this);
         view.getEditor().getGuides().addEventListener(GGuides.InvalidationRequestEvent, this._guidesInvalidationRequest, this);
     }
     GObject.inherit(GEditorFrontStage, GStage);
 
+    GEditorFrontStage.MARGIN_OUTLINE = new GRGBColor([255, 0, 255]);
+
     /** @override */
     GEditorFrontStage.prototype.release = function () {
+        this._view.getScene().removeEventListener(GScene.InvalidationRequestEvent, this._sceneInvalidationRequest, this);
         this._view.getScene().removeEventListener(GNode.AfterPropertiesChangeEvent, this._sceneAfterPropertiesChanged, this);
         this._view.getEditor().removeEventListener(GGuides.InvalidationRequestEvent, this._guidesInvalidationRequest, this);
         this._view.removeEventListener(GMouseEvent.Release, this._cleanGuides, this);
@@ -23,6 +27,14 @@
 
     /** @override */
     GEditorFrontStage.prototype.paint = function (context) {
+        var scene = this._view.getScene();
+
+        if (scene instanceof GPage) {
+            if (context.configuration.pageDecoration && scene.isPaintable(context)) {
+                this._renderPageDecoration(context);
+            }
+        }
+
         this._view.getEditor().getGuides().paint(this._view.getWorldTransform(), context);
     };
 
@@ -54,6 +66,25 @@
 
     GEditorFrontStage.prototype._cleanGuides = function (event) {
         this._view.getEditor().getGuides().invalidate();
+    };
+
+    /**
+     * @param context
+     * @private
+     */
+    GEditorFrontStage.prototype._renderPageDecoration = function (context) {
+        var page = this._view.getScene();
+        var transform = this._view.getWorldTransform();
+        // Get page rectangle and transform it into world space
+        var pageRect = new GRect(page.getProperty('x'), page.getProperty('y'), page.getProperty('w'), page.getProperty('h'));
+        var marginRect = pageRect.expanded(-page.getProperty('ml'), -page.getProperty('mt'), -page.getProperty('mr'), -page.getProperty('mb'));
+        var transformedMarginRect = transform.mapRect(marginRect).toAlignedRect();
+        var mx = transformedMarginRect.getX(), my = transformedMarginRect.getY(), mw = transformedMarginRect.getWidth(), mh = transformedMarginRect.getHeight();
+
+        // Paint margin rect
+        if (!GRect.equals(pageRect, marginRect)) {
+            context.canvas.strokeRect(mx + 0.5, my + 0.5, mw, mh, 1, GEditorFrontStage.MARGIN_OUTLINE);
+        }
     };
 
     /** @override */
